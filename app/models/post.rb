6b4afa2a4b9ee50b23e020a53595d48e7392056d
Henok Kirubel <henokhm2@gmail.com>
2023-10-27 08:38:50 +0300

    Add method to get last 5 comments on a post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
+
+  def self.last_five_comments(id)
+    post = Post.find_by(id:)
+    post.comments.order(created_at: :desc).limit(5)
+  end
 end
