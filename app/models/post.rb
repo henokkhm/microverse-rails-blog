7033e1932d20f33ed085503be811598ba522d559
Henok Kirubel <henokhm2@gmail.com>
2023-10-27 08:56:23 +0300

    Add methods that update counters

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,16 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
 
+  after_save :update_posts_counter
+
   def self.last_five_comments(id)
     post = Post.find_by(id:)
     post.comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_posts_counter
+    user.update(posts_counter: user.posts.count)
   end
 end
