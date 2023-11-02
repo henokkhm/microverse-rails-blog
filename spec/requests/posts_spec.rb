1bc81098cbca4fce3d1d498db3ee7a9fbb3a1ba8
Henok Kirubel <henokhm2@gmail.com>
2023-11-02 22:16:39 +0300

    Add tests for /users/:user_id/posts/:post_id route

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -19,4 +19,24 @@ RSpec.describe 'Posts contoller', type: :request do
       expect(response.body).to include('<h1>List of All Posts Made by a Specific User</h1>')
     end
   end
+
+  describe 'GET request to endpoint "/users/:user_id/posts/:post_id"' do
+    before :each do
+      @user = User.create(name: 'Henok', posts_counter: 0)
+      @post = @user.posts.create(title: 'Post 1')
+      get user_post_path(@user, @post)
+    end
+
+    it 'returns successful response' do
+      expect(response).to be_successful
+    end
+
+    it 'renders the correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the right placeholder HTML element in the response' do
+      expect(response.body).to include('<h1>Details Page for a Specific Post Made by a Specific User</h1>')
+    end
+  end
 end
