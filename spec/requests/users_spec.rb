1f40e46b39ba3db60af222e15f4c101b7fd01426
Henok Kirubel <henokhm2@gmail.com>
2023-11-02 21:17:27 +0300

    Add tests for the /users/:id route

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -18,4 +18,23 @@ RSpec.describe 'Users controller', type: :request do
       expect(response.body).to include('<h1>List of All Users</h1>')
     end
   end
+
+  describe 'GET request to endpoint "/users/:id"' do
+    before :each do
+      @user = User.create(name: 'Henok', posts_counter: 0)
+      get user_path(@user)
+    end
+
+    it 'return successful response' do
+      expect(response).to be_successful
+    end
+
+    it 'renders the correct template' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the right placeholder HTML element in the response' do
+      expect(response.body).to include('<h1>Details Page for a Specific User</h1>')
+    end
+  end
 end
