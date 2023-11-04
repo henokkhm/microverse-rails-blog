ce3c888c12a3618037e0f5de3a404285edf82746
Henok Kirubel <henokhm2@gmail.com>
2023-11-04 20:18:45 +0300

    Pass user to single post page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,8 @@ class PostsController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
+    @comment = Comment.new
   end
 end
