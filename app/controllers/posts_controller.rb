3c53788edebd12f977dec6eda685bfe564dc8c87
Henok Kirubel <henokhm2@gmail.com>
2023-11-04 21:06:15 +0300

    Add controlle action for adding new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,26 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comment = Comment.new
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+
+    if @post.save
+      flash[:success] = 'Your post has been saved!'
+      redirect_to user_path(id: @user.id)
+    else
+      flash[:error] = 'Unable to save comment. Please try later.'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
