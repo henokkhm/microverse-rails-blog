0e11e15fa88dc426a088059a4b924669d2fa8c8a
Henok Kirubel <henokhm2@gmail.com>
2023-11-04 20:17:48 +0300

    Add comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @post = Post.find(params[:post_id])
+    @comment.user = User.find(params[:user_id])
+    @comment.post_id = @post.id
+
+    if @comment.save
+      flash[:success] = 'Your comment has been saved!'
+      redirect_to user_post_path(user_id: @post.author, id: @post.id)
+    else
+      flash[:error] = 'Unable to save comment. Please try later.'
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
