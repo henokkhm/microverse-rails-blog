69c0ef2e3414d679399abee0f180e7cf20d6ea02
wolterbwambale <benplaan1@gmail.com>
2023-11-07 19:38:23 +0300

    add the post should modules

--- spec/requests/integration/posts/post_show_spec.rb
+++ spec/requests/integration/posts/post_show_spec.rb
@@ -23,4 +23,38 @@ RSpec.describe 'post show view page', type: :system do
   let!(:comment3) do
     Comment.create(post: post1, user: user1, text: 'good!')
   end
+
+  describe 'show correct post for user1' do
+    before(:example) do
+      visit user_post_path(user1, post1)
+    end
+
+    it 'displays the post title' do
+      expect(page).to have_content(post1.title)
+    end
+
+    it 'displays the number of likes' do
+      expect(page).to have_content("#{post1.likes_counter} Likes")
+    end
+
+    it 'displays the number of comments' do
+      expect(page).to have_content("#{post1.comments_counter} Comments")
+    end
+
+    it 'displays the username of each commenter' do
+      expect(page).to have_content(comment1.user.name)
+      expect(page).to have_content(comment2.user.name)
+      expect(page).to have_content(comment3.user.name)
+    end
+
+    it 'displays the post body' do
+      expect(page).to have_content(post1.text)
+    end
+
+    it 'displays the comment left by each commenter' do
+      expect(page).to have_content(comment1.text)
+      expect(page).to have_content(comment2.text)
+      expect(page).to have_content(comment3.text)
+    end
+  end
 end
