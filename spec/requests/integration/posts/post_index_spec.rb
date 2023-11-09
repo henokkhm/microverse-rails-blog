ce456e31bf0a2fe7c68e387d5470e4b8df529b2a
Henok Kirubel <henokhm2@gmail.com>
2023-11-09 03:14:19 +0300

    Use db seed for post index page tests

--- spec/requests/integration/posts/post_index_spec.rb
+++ spec/requests/integration/posts/post_index_spec.rb
@@ -1,92 +1,51 @@
 require 'rails_helper'
 
-RSpec.describe 'post index view page', type: :system do
-  let!(:user1) do
-    User.create(
-      name: 'test user1',
-      photo: 'https://example.com/user1.jpg', 
-      bio: 'test_bio1',
-      posts_counter: 1
-    )
+# The following tests assume that the seeds of the database in /db/seeds.rb have been
+# added to the database. This can be done by running the 'bin/rails db:reset' command
+RSpec.describe "Oscar's post index page" do
+  before :each do
+    oscar = User.find(4) # Oscar has id of 4
+    puts "path is #{user_posts_path(oscar)}"
+    visit user_posts_path(oscar)
   end
 
-  let!(:post1) do
-    Post.create(author: user1, title: 'Post 1', text: 'Post 1 content', comments_counter: 1, likes_counter: 0)
+  it "displays Oscar's profile picture" do
+    expect(page).to have_selector("img[src='#{oscar.photo}']")
   end
 
-  let!(:post2) do
-    Post.create(author: user1, title: 'Post 2', text: 'Post 2 content', comments_counter: 1, likes_counter: 0)
+  it "displays Oscar's username" do
+    expect(page).to have_content('Oscar')
   end
 
-  let!(:comment1) do
-    Comment.create(post: post1, user: user1, text: 'nice!')
+  it 'displays the number of posts Oscar has' do
+    expect(page).to have_content('Number of posts: 4')
   end
 
-  let!(:comment2) do
-    Comment.create(post: post1, user: user1, text: 'great!')
+  it "displays Oscar's first post's title" do
+    expect(page).to have_content("Oscar's first post")
   end
 
-  describe 'show correct posts for user1' do
-    before(:example) do
-      visit user_posts_path(user1)
-    end
-    it 'displays the user\'s username' do
-      expect(page).to have_content(user1.name)
-    end
+  it "displays some of Oscar's first post's body" do
+    expect(page).to have_content("Oscar's first post...")
+  end
 
-    it 'displays the user\'s profile picture' do
-      expect(page).to have_selector("img[src='#{user1.photo}']")
-    end
+  it "displays the first few comments on Oscar's first post" do
+    expect(page).to have_content("Alice's first comment")
+    expect(page).to have_content("Tom's second comment")
+  end
 
-    it 'displays the number of posts the user has written' do
-      expect(page).to have_content("Number of posts: #{user1.posts_counter}")
-    end
-    it 'displays the first comments on a post' do
-      expect(page).to have_content(comment1.text)
-    end
-    
-    it 'displays a post\'s title and text' do
-        expect(page).to have_content(post1.title)
-        expect(page).to have_content(post1.text)
-        expect(page).to have_content(post2.title)
-        expect(page).to have_content(post2.text)
-      end
-  
-      it 'displays the number of comments a post has' do
-        # Visit the user's posts page
-        visit user_posts_path(user1)
-  
-        expect(page).to have_content(post1.title)
-        expect(page).to have_content(post1.text)
-  
-        # Check the comments count in the updated format
-        expect(page).to have_content("#{post1.comments_counter} Comments")
-      end
-      
-      it 'displays the number of likes a post has' do
-        # Visit the user's posts page
-        visit user_posts_path(user1)
-  
-        # Check for the post's title and content
-        expect(page).to have_content(post1.title)
-        expect(page).to have_content(post1.text)
-  
-        expect(page).to have_content("#{post1.likes_counter} Likes")
-      end
-  
-      it 'redirects to the post show page when a post is clicked' do
-        click_link post1.title
-        expect(page).to have_current_path(user_post_path(user1, post1))
-        expect(page).to have_content(post1.title)
-      end
-  
-      it 'displays a section for pagination if there are more posts' do
-        # Set the user's posts_counter to a value that exceeds the page limit
-        user1.update(posts_counter: 10)
-  
-        visit user_path(user1)
-        expect(page).to have_selector('.button-container')
-      end
-  
-    end
+  it "displays how many comments Oscar's first post has" do
+    expect(page).to have_content('Comments: 2')
+  end
+
+  it "displays how many likes Oscar's first post has" do
+    expect(page).to have_content('Likes: 3')
+  end
+
+  it "redirects to the post's show page when user clicks on Oscar's first post's card" do
+    oscars_first_post = Post.where(title: "Oscar's first post")
+    click_link oscars_first_post.title
+    expect(page).to have_current_path(user_post_path(oscar, oscars_first_post))
+    expect(page).to have_content("Oscar's first post")
+  end
 end
