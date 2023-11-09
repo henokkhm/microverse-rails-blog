33b06979dffe9615f31b8b125c192a96bdd592e3
Henok Kirubel <henokhm2@gmail.com>
2023-11-10 00:27:48 +0300

    Update failing tests for new seeds data

--- spec/requests/integration/posts/post_index_spec.rb
+++ spec/requests/integration/posts/post_index_spec.rb
@@ -19,36 +19,36 @@ RSpec.describe 'Post index page', type: :system do
     end
 
     it 'displays the number of posts Oscar has' do
-      expect(page).to have_content('Number of posts: 4')
+      expect(page).to have_content('Number of posts: 9')
     end
 
-    it "displays Oscar's first post's title" do
-      expect(page).to have_content("Oscar's first post")
+    it "displays Oscar's latest post's title" do
+      expect(page).to have_content("Oscar's ninth post")
     end
 
-    it "displays some of Oscar's first post's body" do
-      expect(page).to have_content("Oscar's first post...")
+    it "displays some of Oscar's latest post's body" do
+      expect(page).to have_content("Oscar's ninth post...")
     end
 
-    it "displays the first few comments on Oscar's first post" do
-      expect(page).to have_content("Alice's first comment")
-      expect(page).to have_content("Tom's second comment")
+    it "displays the first few comments on Oscar's latest post" do
+      expect(page).to have_content("Linda's sixth comment")
+      expect(page).to have_content("Alice's fourth comment")
     end
 
-    it "displays how many comments Oscar's first post has" do
+    it "displays how many comments Oscar's latest post has" do
       expect(page).to have_content('Comments: 2')
     end
 
-    it "displays how many likes Oscar's first post has" do
+    it "displays how many likes Oscar's latest post has" do
       expect(page).to have_content('Likes: 3')
     end
 
-    it "redirects to the post's show page when user clicks on Oscar's first post's card" do
+    it "redirects to the post's show page when user clicks on Oscar's latest post's card" do
       oscar = User.find(4) # Oscar has id of 4
-      oscars_first_post = Post.where(title: "Oscar's first post").first
-      click_link oscars_first_post.title
-      expect(page).to have_current_path(user_post_path(oscar, oscars_first_post))
-      expect(page).to have_content("Oscar's first post")
+      oscars_ninth_post = Post.where(title: "Oscar's ninth post").first
+      click_link oscars_ninth_post.title
+      expect(page).to have_current_path(user_post_path(oscar, oscars_ninth_post))
+      expect(page).to have_content("Oscar's ninth post")
       expect(page).to have_content('New comment')
     end
   end
